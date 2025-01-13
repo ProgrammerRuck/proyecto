ed1e0f0f86ee5c2923e7a671e389973010aeec03
Jorge <BusinessJV24@gmail.com>
2025-01-13 18:26:00 -0300

    Camino 2.

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,9 +1,17 @@
+import { useState } from 'react';
 export default function Intro(props){
+    const [ counter, setCounter ] = useState(0);
+
     console.log(props);
     const { message } = props;
     return(
     <div>
         <p>Hey</p>
         <p>{ message }</p>
+        <h4>El numero de clicks es: {counter} </h4>
+        <h2>Contador con Hooks</h2>
+         <button type="submit" onClick={() => setCounter(counter + 1)}>
+            Sumar :D
+         </button>
     </div>)
 }
