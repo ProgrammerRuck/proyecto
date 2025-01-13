b32c2e40dd37eb77271dc25380fc6a06f3223f7e
Jorge <BusinessJV24@gmail.com>
2025-01-13 18:50:05 -0300

    Camino 3.

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,17 +1,29 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
 export default function Intro(props){
     const [ counter, setCounter ] = useState(0);
-
-    console.log(props);
+    const [ semaforo, setSemaforo ] = useState(false);
     const { message } = props;
+
+        useEffect(() => {
+            console.log(semaforo);
+        },[semaforo]); 
+
+    const contar = () => {
+        setCounter(counter+ 1);
+        setSemaforo(!semaforo);
+    }
+    
     return(
     <div>
         <p>Hey</p>
         <p>{ message }</p>
         <h4>El numero de clicks es: {counter} </h4>
+        <p>El semaforo se pondra de color {semaforo ? "rojo" : "verde"} </p>
         <h2>Contador con Hooks</h2>
-         <button type="submit" onClick={() => setCounter(counter + 1)}>
-            Sumar :D
+         <button type="submit" onClick={contar}>
+            Sumar ++
          </button>
-    </div>)
+    </div>
+    );
 }
